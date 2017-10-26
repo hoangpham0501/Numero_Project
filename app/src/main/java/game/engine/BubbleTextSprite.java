eb98363eb1986ceee72be2feebaf1cae23641ba1
Hoang Pham <hoang.a3.lqd@gmail.com>
2017-10-26 19:46:45 +0700

    version 2

---
+++ app/src/main/java/game/engine/BubbleTextSprite.java
@@ -0,0 +1,41 @@
+package game.engine;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+public class BubbleTextSprite extends Sprite {
+    public String text;
+    public int deltaBubbleSizeX;
+    public int deltaBubbleSizeY;
+    public BubbleTextSprite(Bitmap image, String text, int width, int height, int bbSizeX, int bbSizeY) {
+        super(image, width, height);
+        this.text = text;
+        this.deltaBubbleSizeX = bbSizeX;
+        this.deltaBubbleSizeY = bbSizeY;
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(40);
+        paint.setShadowLayer(6, 0, 0, Color.WHITE);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void render(Canvas c) {
+        if(steps-- > 0){
+//		   System.out.println("x: "+x+" y: "+y+" deltaX: "+deltaX+" deltaY: "+deltaY);
+            x+=deltaX;
+            y+=deltaY;
+        }
+        src.left = currentHorFrame*width;
+        src.top = (int) (currentVerFrame*height);
+        src.right = currentHorFrame*width + width;
+        src.bottom = (int) (currentVerFrame*height +height);
+        dist.left = (int)x;
+        dist.top = (int) y;
+        dist.bottom = (int)y+ dispHeight;
+        dist.right =  (int)x+dispWidth ;
+        c.drawBitmap(image,src,dist, paint);
+        c.drawText(text,(float)(x+deltaX+deltaBubbleSizeX),(float)(y+deltaY+deltaBubbleSizeY) , paint);
+
+    }
+
+}
---
+++ app/src/main/java/game/engine/TextSprite.java
@@ -0,0 +1,32 @@
+package game.engine;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+public class TextSprite extends Sprite {
+	public String text;
+	public TextSprite(Bitmap image, String text, int width, int height, int color) {
+		super(image, width, height);
+		this.text = text;
+		paint.setColor(color);
+		paint.setTextSize(40);
+		paint.setShadowLayer(6, 0, 0, Color.WHITE);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setColor (int color) {
+        paint.setColor(color);
+    }
+
+	@Override
+	public void render(Canvas c) {
+//		c.drawText(text,(int) x, (int)y, (float)x+dispWidth, (float)y+dispHeight,paint);
+//		paint.setColor(Color.MAGENTA);
+//		System.out.println("gere");
+//		System.out.println(text+" "+x+" "+y+" "+paint);
+		c.drawText(text,(float)(x+width),(float)(y+height) , paint);
+		
+	}
+	
+}
